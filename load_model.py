4fb52c59d97114cc15819ea1f863087956760492
Pranava Bhat <72594385+pranavabhat-5204@users.noreply.github.com>
2025-07-13 00:20:26 +0530

    Update load_model.py

--- load_model.py
+++ load_model.py
@@ -7,6 +7,6 @@ loaded_tokenizer = AutoTokenizer.from_pretrained("my_fine_tuned_t5_small_model")
 loaded_model = AutoModelForSeq2SeqLM.from_pretrained("my_fine_tuned_t5_small_model")
 
 inputs = loaded_tokenizer(prompt, return_tensors="pt", padding=True).to(loaded_model.device)
-output = loaded_model.generate(**inputs, max_new_tokens=1024, do_sample=True, top_p=0.5, temperature=0.5)
+output = loaded_model.generate(**inputs, do_sample=True, top_p=0.5, temperature=0.5)
 print(loaded_tokenizer.decode(output[0], skip_special_tokens=True))
 #the output for the above input was """We’re proud to unveil the latest fine-tuned versions of Llama 3, purpose-built for tool use and function calling—and they’re already making waves. These models now rank #1 on the Berkeley Function Calling Leaderboard, outperforming every other open-source and proprietary model out there. Crafted on synthetic data and optimized for real-world developer needs, this release wouldn’t be possible without the incredible collaboration between Groq and Glaive teams—huge kudos to everyone involved!""" 
